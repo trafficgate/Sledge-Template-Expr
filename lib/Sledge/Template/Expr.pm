be7ae2fb4649681abcd0414d155b9ce7ddbc44c7
miyagawa <>
2003-02-14 07:57:56 +0000

    added PREREQ added POD

--- lib/Sledge/Template/Expr.pm
+++ lib/Sledge/Template/Expr.pm
@@ -38,3 +38,44 @@ sub register_function {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+Sledge::Template::Expr - HTML::Template::Expr adapter
+
+=head1 SYNOPSIS
+
+  package Your::Pages;
+  use Sledge::Template::Expr;
+
+=head1 DESCRIPTION
+
+テンプレートとして HTML::Template::Expr を利用するためのプラグインです。
+
+=head1 METHODS
+
+以下のメソッドがtemplateオブジェクトから利用可能です。
+
+=over 4
+
+=item register_function
+
+  $page->tmpl->register_function(add => sub { $_[0] + $_[1] });
+
+HTML::Tempalte::Expr の C<Cregister_function> へのwrapperです。
+
+=back
+
+=head1 AUTHOR
+
+Tatsuhiko Miyagawa with Sledge development team.
+
+=head1 SEE ALSO
+
+L<HTML::Template::Expr>, L<Sledge::Template>, L<Sledge::Template::TT>
+
+=cut
+
+
